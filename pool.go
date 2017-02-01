e5f7f0d009e9c66befe4e29039aab85bcf57b946
itsMontoya <josh@montoya.io>
2017-02-01 09:29:25 -0800

    Re-implement sync.Pool to compare against using mailbox

--- pool.go
+++ pool.go
@@ -1,34 +1,45 @@
 package jsoon
 
 import (
-	"github.com/itsmontoya/mailbox"
+	//	"github.com/itsmontoya/mailbox"
+	"sync"
 )
 
 func newPool(sz int) *pool {
 	var p pool
-	p.mb = mailbox.New(sz)
+	//p.mb = mailbox.New(sz)
+	p.p = sync.Pool{
+		New: func() interface{} {
+			return newBuffer()
+		},
+	}
 	return &p
 }
 
 type pool struct {
-	mb *mailbox.Mailbox
+	//	mb *mailbox.Mailbox
+	p sync.Pool
 }
 
 // Acquire will acquire a buffer from the pool
 func (p *pool) Acquire() (buf *buffer) {
 	var (
-		v  interface{}
-		sc mailbox.StateCode
+		//	v interface{}
+		//		sc mailbox.StateCode
 		ok bool
 	)
 
-	if v, sc = p.mb.Receive(false); sc == mailbox.StateEmpty {
-		return newBuffer()
-	}
-
-	if buf, ok = v.(*buffer); !ok {
+	if buf, ok = p.p.Get().(*buffer); !ok {
 		panic("invalid pool type")
 	}
+
+	//	if v, sc = p.mb.Receive(false); sc == mailbox.StateEmpty {
+	//		return newBuffer()
+	//	}
+
+	//	if buf, ok = v.(*buffer); !ok {
+	//		panic("invalid pool type")
+	//	}
 
 	return
 }
@@ -36,6 +47,7 @@ func (p *pool) Acquire() (buf *buffer) {
 // Release will release a buffer to the pool
 func (p *pool) Release(buf *buffer) {
 	buf.Reset()
+	p.p.Put(buf)
 	// If the mailbox is full, move along
-	p.mb.Send(buf, false)
+	//	p.mb.Send(buf, false)
 }
