f50f44e3c0dbdca0a3b528ccdfa075d5f8ff90bc
itsMontoya <josh@montoya.io>
2017-02-01 11:12:02 -0800

    Adding pool for array encoder

--- pool.go
+++ pool.go
@@ -9,6 +9,13 @@ func newPool() *pool {
 			return newBuffer()
 		},
 	}
+
+	p.aep = sync.Pool{
+		New: func() interface{} {
+			return &ArrayEncoder{}
+		},
+	}
+
 	return &p
 }
 
@@ -32,4 +39,21 @@ func (p *pool) Acquire() (buf *buffer) {
 func (p *pool) Release(buf *buffer) {
 	buf.Reset()
 	p.p.Put(buf)
+}
+
+// AcquireAE will acquire an array encoder from the pool
+func (p *pool) AcquireAE(e *Encoder) (ae *ArrayEncoder) {
+	var ok bool
+	if ae, ok = p.aep.Get().(*ArrayEncoder); !ok {
+		panic("invalid pool type")
+	}
+
+	ae.e = e
+	return
+}
+
+// ReleaseAE will release an array encoder to the pool
+func (p *pool) ReleaseAE(ae *ArrayEncoder) {
+	ae.e = nil
+	p.aep.Put(ae)
 }
