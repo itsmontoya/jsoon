a31121565587979637ffb39a84273ba46708aaeb
Josh Montoya <josh@montoya.io>
2025-07-15 13:53:19 -0700

    use stdlib errors

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/itsmontoya/jsoon
 
 go 1.24.0
 
-require (
-	github.com/buger/jsonparser v1.1.1
-	github.com/missionMeteora/toolkit v0.0.0-20170713173850-88364e3ef8cc
-)
+require github.com/buger/jsonparser v1.1.1
--- jsoon.go
+++ jsoon.go
@@ -1,27 +1,27 @@
 package jsoon
 
-import "github.com/missionMeteora/toolkit/errors"
+import "errors"
 
-const (
+var (
 	// ErrInvalidChar is returned when an invalid character is found within a provided json
-	ErrInvalidChar = errors.Error("invalid character")
+	ErrInvalidChar = errors.New("invalid character")
 	// ErrUnexpectedEnd is returned when decoding data ends before the json is complete
-	ErrUnexpectedEnd = errors.Error("input ended before expected")
+	ErrUnexpectedEnd = errors.New("input ended before expected")
 	//ErrInvalidValue is returned when a Decodee is not present for an object or a ArrayDecodee is not present for an array
-	ErrInvalidValue = errors.Error("invalid value provided")
+	ErrInvalidValue = errors.New("invalid value provided")
 
 	// ErrValueNotObject is returned when value is not an object
-	ErrValueNotObject = errors.Error("value cannot be parsed as an object")
+	ErrValueNotObject = errors.New("value cannot be parsed as an object")
 	// ErrValueNotArray is returned when value is not an array
-	ErrValueNotArray = errors.Error("value cannot be parsed as an array")
+	ErrValueNotArray = errors.New("value cannot be parsed as an array")
 	// ErrValueNotString is returned when value is not a string
-	ErrValueNotString = errors.Error("value cannot be parsed as a string")
+	ErrValueNotString = errors.New("value cannot be parsed as a string")
 	// ErrValueNotBytes is returned when value is not bytes
-	ErrValueNotBytes = errors.Error("value cannot be parsed as bytes")
+	ErrValueNotBytes = errors.New("value cannot be parsed as bytes")
 	// ErrValueNotNumber is returned when value is not a number
-	ErrValueNotNumber = errors.Error("value cannot be parsed as a number")
+	ErrValueNotNumber = errors.New("value cannot be parsed as a number")
 	// ErrValueNotBool is returned when value is not a boolean
-	ErrValueNotBool = errors.Error("value cannot be parsed as a boolean")
+	ErrValueNotBool = errors.New("value cannot be parsed as a boolean")
 )
 
 const (
